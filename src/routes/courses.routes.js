c35d845f2c22c4287082c7c949d5d71d32aced0d
Elvin Mendez <[elvinmanuel005@gmail.com]>
2026-02-07 16:39:55 -0400

    updated

--- src/routes/courses.routes.js
+++ src/routes/courses.routes.js
@@ -1,6 +1,6 @@
 // courses.routes.js
 import { Router } from 'express';
-import { getCourses, createCourse } from '../controllers/courses.controller.js';
+import { getCourses, createCourse, getCourse, updateCourse, deleteCourse } from '../controllers/courses.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 import { roleMiddleware } from '../middlewares/role.middleware.js';
 
@@ -9,6 +9,9 @@ const router = Router();
 router.use(authMiddleware);
 
 router.get('/', getCourses);
+router.get('/:id', getCourse);
 router.post('/', roleMiddleware('admin'), createCourse);
+router.put('/:id', roleMiddleware('admin'), updateCourse);
+router.delete('/:id', roleMiddleware('admin'), deleteCourse);
 
 export default router;
